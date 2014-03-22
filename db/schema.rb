f3793335ca32e8f5da2ee271a911639803942df2
Gregory Sheppard <gregory.sheppard@live.com>
2014-03-22 11:09:49 -0400

    Added Actors model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322150026) do
+ActiveRecord::Schema.define(version: 20140322150919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "musicals", force: true do |t|
     t.string   "name",       null: false
